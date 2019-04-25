80796136d7e756465b4cc7b9085fc8e40ce899d3
Brian Curran <curranbriant@gmail.com>
2019-04-25 07:27:47 -0400

    first half of balance page

--- App.js
+++ App.js
@@ -8,19 +8,24 @@ import {
   Button,
   Icon,
   Title,
+  Content,
+  Text,
+  Card,
+  CardItem,
 } from 'native-base';
+
 export default class HeaderExample extends Component {
   render() {
     return (
       <Container>
-        <Header>
+        <Header transparent>
           <Left>
             <Button transparent>
               <Icon name='arrow-back' />
             </Button>
           </Left>
           <Body>
-            <Title>Header</Title>
+            <Title transparent>Header</Title>
           </Body>
           <Right>
             <Button transparent>
@@ -28,6 +33,115 @@ export default class HeaderExample extends Component {
             </Button>
           </Right>
         </Header>
+        <Content>
+          <Container
+            style={{
+              height: 290,
+            }}
+          >
+            <Container
+              style={{
+                backgroundColor: '#9377DE',
+                borderTopRightRadius: 0,
+                borderTopLeftRadius: 0,
+                borderRadius: 12,
+                borderColor: '#E91E63',
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 25,
+                  color: 'white',
+                  paddingLeft: 30,
+                  paddingTop: 30,
+                }}
+              >
+                My Balance
+              </Text>
+              <Text
+                style={{
+                  fontSize: 70,
+                  color: 'white',
+                  paddingLeft: 20,
+                  paddingTop: 5,
+                }}
+              >
+                $240,465
+              </Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: 'white',
+                  paddingLeft: 30,
+                  paddingTop: 0,
+                }}
+              >
+                Tue, April 22, 2019
+              </Text>
+              <Container
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'center',
+                  backgroundColor: 'transparent',
+                  marginTop: 85,
+                }}
+              >
+                <Card
+                  style={{
+                    width: 350,
+                    height: 100,
+                    marginLeft: 10,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    paddingTop: 0,
+                    borderRadius: 4,
+                  }}
+                >
+                  <Content
+                    style={{
+                      marginLeft: 35,
+                    }}
+                  >
+                    <CardItem>
+                      <Text>Income</Text>
+                    </CardItem>
+                    <CardItem>
+                      <Text
+                        style={{
+                          color: '#4B37FF',
+                          fontSize: 25,
+                        }}
+                      >
+                        $4,349
+                      </Text>
+                    </CardItem>
+                  </Content>
+                  <Content style={{}}>
+                    <CardItem>
+                      <Body>
+                        <Text>Spent</Text>
+                      </Body>
+                    </CardItem>
+                    <CardItem>
+                      <Body>
+                        <Text
+                          style={{
+                            color: '#4B37FF',
+                            fontSize: 25,
+                          }}
+                        >
+                          $3,345
+                        </Text>
+                      </Body>
+                    </CardItem>
+                  </Content>
+                </Card>
+              </Container>
+            </Container>
+          </Container>
+        </Content>
       </Container>
     );
   }
