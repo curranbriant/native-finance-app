62f3195c54cb2c899a06846789a98ff3e457e40e
Brian Curran <curranbriant@gmail.com>
2019-04-26 14:19:37 -0400

    added savings calculator

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { AppRegistry, ScrollView, Image } from 'react-native';
 import {
   Container,
   Header,
@@ -22,184 +23,230 @@ import {
   StackedBarChart,
 } from 'react-native-chart-kit';
 import { Svg } from 'expo';
+import { group } from '/Users/briancurran/personalProjects/nativeProjects/bankApp/seed.json';
+const persons = group.account;
 
-export default class HeaderExample extends Component {
+export default class App extends Component {
   render() {
     return (
-      <Container>
-        <Header transparent>
-          <Left>
-            <Button transparent>
-              <Icon name='arrow-back' />
-            </Button>
-          </Left>
-          <Body>
-            <Title transparent />
-          </Body>
-        </Header>
-        <Content>
-          <Container
-            style={{
-              height: 290,
-            }}
-          >
+      <ScrollView>
+        <Container style={{}}>
+          <Header transparent>
+            <Left>
+              <Button transparent>
+                <Icon name='arrow-back' />
+              </Button>
+            </Left>
+            <Body>
+              <Title transparent />
+            </Body>
+          </Header>
+          <Content>
             <Container
               style={{
-                backgroundColor: '#9377DE',
-                borderTopRightRadius: 0,
-                borderTopLeftRadius: 0,
-                borderRadius: 12,
-                borderColor: '#E91E63',
+                height: 825,
               }}
             >
-              <Text
-                style={{
-                  fontSize: 25,
-                  color: 'white',
-                  paddingLeft: 30,
-                  paddingTop: 30,
-                }}
-              >
-                My Balance
-              </Text>
-              <Text
-                style={{
-                  fontSize: 70,
-                  color: 'white',
-                  paddingLeft: 20,
-                  paddingTop: 5,
-                }}
-              >
-                $240,465
-              </Text>
-              <Text
-                style={{
-                  fontSize: 20,
-                  color: 'white',
-                  paddingLeft: 30,
-                  paddingTop: 0,
-                }}
-              >
-                Thr, April 25, 2019
-              </Text>
               <Container
                 style={{
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  backgroundColor: 'transparent',
-                  marginTop: 85,
+                  backgroundColor: '#9377DE',
+                  borderTopRightRadius: 0,
+                  borderTopLeftRadius: 0,
+                  borderRadius: 12,
+                  borderColor: '#E91E63',
                 }}
               >
-                <Card
+                <Text
                   style={{
-                    width: 350,
-                    height: 100,
-                    marginLeft: 10,
-                    justifyContent: 'center',
-                    alignItems: 'center',
-                    flexDirection: 'row',
-                    alignItems: 'center',
-                    paddingTop: 0,
-                    borderRadius: 4,
+                    fontSize: 25,
+                    color: 'white',
+                    paddingLeft: 30,
+                    paddingTop: 30,
                   }}
                 >
-                  <Content
+                  My Balance
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 70,
+                    color: 'white',
+                    paddingLeft: 20,
+                    paddingTop: 5,
+                  }}
+                >
+                  $240,465
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: 'white',
+                    paddingLeft: 30,
+                    paddingTop: 0,
+                  }}
+                >
+                  Thr, April 25, 2019
+                </Text>
+                <Container
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                    backgroundColor: 'transparent',
+                    marginTop: 0,
+                  }}
+                >
+                  <Card
                     style={{
-                      marginLeft: 35,
+                      width: 350,
+                      height: 100,
+                      marginLeft: 10,
+                      justifyContent: 'center',
+                      alignItems: 'center',
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      marginTop: 60,
+                      borderRadius: 4,
                     }}
                   >
-                    <CardItem>
-                      <Text>Income</Text>
-                    </CardItem>
-                    <CardItem>
-                      <Text
-                        style={{
-                          color: '#4B37FF',
-                          fontSize: 25,
-                        }}
-                      >
-                        $3,349
-                      </Text>
-                    </CardItem>
-                  </Content>
-                  <Content style={{}}>
-                    <CardItem>
-                      <Body>
-                        <Text>Spent</Text>
-                      </Body>
-                    </CardItem>
-                    <CardItem>
-                      <Body>
+                    <Content
+                      style={{
+                        marginLeft: 35,
+                      }}
+                    >
+                      <CardItem>
+                        <Text>Income</Text>
+                      </CardItem>
+                      <CardItem>
                         <Text
                           style={{
                             color: '#4B37FF',
                             fontSize: 25,
                           }}
                         >
-                          $2,190
+                          $3,349
                         </Text>
-                      </Body>
-                    </CardItem>
-                  </Content>
+                      </CardItem>
+                    </Content>
+                    <Content style={{}}>
+                      <CardItem>
+                        <Body>
+                          <Text>Spent</Text>
+                        </Body>
+                      </CardItem>
+                      <CardItem>
+                        <Body>
+                          <Text
+                            style={{
+                              color: '#4B37FF',
+                              fontSize: 25,
+                            }}
+                          >
+                            $2,190
+                          </Text>
+                        </Body>
+                      </CardItem>
+                    </Content>
+                  </Card>
+                </Container>
+              </Container>
+              {/* <Container
+                style={{
+                  backgroundColor: 'transparent',
+                }}
+              >
+                <Card
+                  style={{
+                    width: 350,
+                    height: 100,
+                    marginLeft: 36,
+                    justifyContent: 'center',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    marginTop: 58,
+                    borderRadius: 4,
+                  }}
+                >
+                  <CardItem>
+                    <Body>
+                      <Text
+                        style={{
+                          fontSize: 20,
+                        }}
+                      >
+                        Amount You Can Put Into Savings
+                      </Text>
+                    </Body>
+                  </CardItem>
+                  <CardItem>
+                    <Body>
+                      <Text
+                        style={{
+                          color: '#4B37FF',
+                          fontSize: 25,
+                        }}
+                      >
+                        $1,239
+                      </Text>
+                    </Body>
+                  </CardItem>
                 </Card>
+              </Container> */}
+              <Container
+                style={{
+                  marginLeft: 7,
+                }}
+              >
+                <Text
+                  style={{
+                    fontSize: 30,
+                    marginLeft: 20,
+                    paddingRight: 10,
+                    color: '#9377DE',
+                  }}
+                >
+                  Stats
+                  <Text
+                    style={{
+                      fontSize: 13,
+                      color: '#9377DE',
+                    }}
+                  >
+                    {'   '}since last pay period
+                  </Text>
+                </Text>
+                <LineChart
+                  data={{
+                    labels: ['3/22', '3/26', '3/29', '4/2', '4/15'],
+                    datasets: [
+                      {
+                        data: [3349, 2000, 2200, 1400, 1600, 1159],
+                      },
+                    ],
+                  }}
+                  width={400}
+                  height={220}
+                  yAxisLabel={'$'}
+                  fromZero={'True'}
+                  withDots={'True'}
+                  chartConfig={{
+                    backgroundGradientFrom: '#fff',
+                    backgroundGradientTo: '#fff',
+                    decimalPlaces: 'none',
+                    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+                    style: {
+                      borderRadius: 16,
+                    },
+                  }}
+                  bezier
+                  style={{
+                    marginVertical: 8,
+                    borderRadius: 16,
+                  }}
+                />
               </Container>
             </Container>
-          </Container>
-        </Content>
-        <Container
-          style={{
-            marginLeft: 7,
-          }}
-        >
-          <Text
-            style={{
-              fontSize: 30,
-              marginLeft: 20,
-              paddingRight: 10,
-              color: '#9377DE',
-            }}
-          >
-            Stats
-            <Text
-              style={{
-                fontSize: 13,
-                color: '#9377DE',
-              }}
-            >
-              {'   '}since last pay period
-            </Text>
-          </Text>
-          <LineChart
-            data={{
-              labels: ['3/22', '3/26', '3/29', '4/2', '4/15'],
-              datasets: [
-                {
-                  data: [3349, 2000, 2200, 1400, 1600, 1159],
-                },
-              ],
-            }}
-            width={400}
-            height={220}
-            yAxisLabel={'$'}
-            fromZero={'True'}
-            withDots={'True'}
-            chartConfig={{
-              backgroundGradientFrom: '#fff',
-              backgroundGradientTo: '#fff',
-              decimalPlaces: 'none',
-              color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-              style: {
-                borderRadius: 16,
-              },
-            }}
-            bezier
-            style={{
-              marginVertical: 8,
-              borderRadius: 16,
-            }}
-          />
+          </Content>
         </Container>
-      </Container>
+      </ScrollView>
     );
   }
 }
